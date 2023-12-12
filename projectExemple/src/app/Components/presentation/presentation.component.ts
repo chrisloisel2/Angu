391d84019a8ae12950de51098fd5ea2ef5e3877a
Christopher Loisel <christopher@MacBook-Pro-de-Christopher.local>
2023-12-12 10:20:03 +0100

    Project angular

---
+++ projectExemple/src/app/Components/presentation/presentation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation',
+  standalone: true,
+  imports: [],
+  templateUrl: './presentation.component.html',
+  styleUrl: './presentation.component.css',
+})
+export class PresentationComponent {}
