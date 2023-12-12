81cc5579cba31fe70a6aee96fdcdc3ddf0755a64
Christopher Loisel <christopher@MacBook-Pro-de-Christopher.local>
2023-12-12 11:17:18 +0100

    deux composants

--- together/src/app/Components/presentation/presentation.component.ts
+++ together/src/app/Components/presentation/presentation.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
+// Html = <app-presentation></app-presentation>
 @Component({
   selector: 'app-presentation',
   standalone: true,
   imports: [],
   templateUrl: './presentation.component.html',
-  styleUrl: './presentation.component.css'
+  styleUrl: './presentation.component.css',
 })
-export class PresentationComponent {
-
-}
+export class PresentationComponent {}
